7ff184dc9feaabf61304a4a1c751f7df1eb17dfd
Tyler Barber <dev@twbarber.com>
2016-11-15 08:31:51 -0600

    CLI input parsing working for happy cases. Needs further testing.

--- cash-register-public/src/main/kotlin/com/twbarber/CashRegisterCliRunner.kt
+++ cash-register-public/src/main/kotlin/com/twbarber/CashRegisterCliRunner.kt
@@ -1,14 +1,44 @@
 package com.twbarber
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
+import java.util.*
 
 class CashRegisterCliRunner {
 
+    private val READY = "ready"
+    private val SHOW_REGEX = "show"
+    private val TAKE_REGEX = "take (\\d+ ){4}\\d+"
+    private val PUT_REGEX = "put (\\d+ ){4}\\d+"
+    private val CHANGE_REGEX = "change \\d+"
+
+    private val register = CashRegister()
+    private val input = Scanner(System.`in`)
+
     fun main() {
         val context = AnnotationConfigApplicationContext(CashRegisterConfig::class.java)
-        val register = CashRegister()
+        System.out.println(READY)
         while (true) {
-
+            val input = input.next().toLowerCase()
+            val out = ""
+            if (input.matches(Regex(SHOW_REGEX))) print(register.show())
+            if (input.matches(Regex(TAKE_REGEX))) print(register.take(parseTransaction(input)))
+            if (input.matches(Regex(PUT_REGEX))) print(register.put(parseTransaction(input)))
+            if (input.matches(Regex(CHANGE_REGEX))) print(register.change(parseChange(input)))
+            print(out)
         }
+    }
+
+    fun parseTransaction(input : String): Balance {
+        val bills = input.split(" ")
+        return Balance(bills[1].toInt(), bills[2].toInt(), bills[3].toInt(),
+                bills[4].toInt(), bills[5].toInt())
+    }
+
+    fun parseChange(input : String): Int {
+        return input.split(" ")[1].toInt()
+    }
+
+    fun print(output : String) {
+        System.out.println(output)
     }
 }
---
+++ cash-register-public/src/test/kotlin/com/twbarber/CashRegisterCliRunnerTest.kt
@@ -0,0 +1,22 @@
+package com.twbarber
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CashRegisterCliRunnerTest {
+
+    val cli = CashRegisterCliRunner()
+
+    @Test
+    fun parseTransaction() {
+        val input = "put 1 1 1 1 1"
+        assertEquals(Balance(1, 1, 1, 1, 1), cli.parseTransaction(input))
+    }
+
+    @Test
+    fun parseChange() {
+        val input = "change 3"
+        assertEquals(3, cli.parseChange(input))
+    }
+
+}
