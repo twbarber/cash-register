a64763f8f302d410527aa36db1bd29753140e09c
Tyler Barber <dev@twbarber.com>
2016-11-15 19:58:40 -0600

    Cash register Testing complete for all but change methods. Need to implement.
    Also need to work on CLI.

--- cash-register-public/src/test/kotlin/com/twbarber/CashRegisterTest.kt
+++ cash-register-public/src/test/kotlin/com/twbarber/CashRegisterTest.kt
@@ -1,27 +1,44 @@
 package com.twbarber
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Created by tyler on 11/13/16.
- */
 class CashRegisterTest {
 
     @Test
     fun show() {
         val register = CashRegister()
+        assertEquals("$0 0 0 0 0 0", register.show())
     }
 
     @Test
     fun put() {
+        val register = CashRegister()
+        register.put(Balance(1, 1, 1, 1, 1))
+        assertEquals("$38 1 1 1 1 1", register.show())
+    }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun negativePutThrowsIllegalArgument() {
+        val register = CashRegister()
+        register.put(Balance(1, 1, 1, 1, -1))
     }
 
     @Test
     fun take() {
+        val register = CashRegister()
+        register.put(Balance(1, 1, 1, 1, 1))
+        assertEquals("$38 1 1 1 1 1", register.show())
+        register.take(Balance(1, 1, 1, 1, 1))
+        assertEquals("$0 0 0 0 0 0", register.show())
+    }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun excessiveTakeThrowsIllegalArgument() {
+        val register = CashRegister()
+        register.put(Balance(1, 1, 1, 1, 1))
+        assertEquals("$38 1 1 1 1 1", register.show())
+        register.take(Balance(2, 2, 2, 2, 2))
     }
 
     @Test
