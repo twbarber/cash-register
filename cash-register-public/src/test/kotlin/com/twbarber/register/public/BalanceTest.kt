87ab742fd9d68d249d7e870f343c23d40713850a
Tyler <tyler@twbarber.com>
2016-11-19 21:26:52 -0600

    Test for allBills method

--- cash-register-public/src/test/kotlin/com/twbarber/register/public/BalanceTest.kt
+++ cash-register-public/src/test/kotlin/com/twbarber/register/public/BalanceTest.kt
@@ -4,6 +4,7 @@ import com.twbarber.register.public.data.Balance
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
+import java.util.*
 
 class BalanceTest {
 
@@ -29,6 +30,25 @@ class BalanceTest {
     fun validToString() {
         val bal = Balance(1, 1, 1, 1, 1)
         assertEquals("1 1 1 1 1", bal.bills())
+    }
+
+    @Test
+    fun allBillsEmpty() {
+        val bal = Balance(0, 0, 0, 0, 0)
+        val bills = ArrayList<Int>()
+        assertEquals(bills, bal.allBills())
+    }
+
+    @Test
+    fun allBills() {
+        val bal = Balance(1, 1, 1, 1, 1)
+        val bills = ArrayList<Int>()
+        bills.add(20)
+        bills.add(10)
+        bills.add(5)
+        bills.add(2)
+        bills.add(1)
+        assertEquals(bills, bal.allBills())
     }
 
     @Test
